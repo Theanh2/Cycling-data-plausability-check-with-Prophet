52426fb28649113e2028af4bc3b6d75a5717fee8
Juntae Kwon <junjun.kwon@gmail.com>
2024-03-08 12:34:33 +0900

    wip

---
+++ python/data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("https://opendata.muenchen.de/dataset/022a11ff-4dcb-4f03-b7dd-a6c94a094587/resource/66be7619-a672-4382-bf88-e3688c5abc2b/download/rad_2008_15min_06_06_23_r.csv")
+
+df.info
